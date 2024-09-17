4818baa30fa9358e6a3d7a7b228a2c1f85709cbf
Luiz Felipe Pereira de Sena <luiz.sena@upflow.me>
2024-09-16 21:17:47 -0300

    fix

--- src/server.ts
+++ src/server.ts
@@ -16,23 +16,23 @@ files.forEach(file => {
         const bubble = applyBubble(instructions);
         const outputFileName = file.replace('.txt', '-RESULTADO.txt');
         writeFile(path.join(directoryDestiny, outputFileName), bubble);
-        /* writeFile(path.join(directory, outputFileName), bubble); */
+        writeFile(path.join(directory, outputFileName), bubble);
     } else if (file.includes('03') || file.includes('04')) {
         const forwading = applyForwading(instructions);
         const outputFileName = file.replace('.txt', '-RESULTADO.txt');
         writeFile(path.join(directoryDestiny, outputFileName), forwading);
-        /* writeFile(path.join(directory, outputFileName), forwading); */
+        writeFile(path.join(directory, outputFileName), forwading);
     } else if (file.includes('05') || file.includes('06') || file.includes('07')) {
         const reordering = applyReordering(instructions);
         const outputFileName = file.replace('.txt', '-RESULTADO.txt');
         writeFile(path.join(directoryDestiny, outputFileName), reordering);
-        /* writeFile(path.join(directory, outputFileName), reordering); */
+        writeFile(path.join(directory, outputFileName), reordering);
     } else if (file.includes('09') || file.includes('10')) {
         const forwarding = applyForwading(instructions);
         const reordering = applyReordering(forwarding);
         const outputFileName = file.replace('.txt', '-RESULTADO.txt');
         writeFile(path.join(directoryDestiny, outputFileName), reordering);
-        /* writeFile(path.join(directory, outputFileName), reordering); */
+        writeFile(path.join(directory, outputFileName), reordering);
     } else {
         console.log(`File ${file} is empty or not exists`);
     }
